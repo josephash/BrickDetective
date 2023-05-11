797ce6ef6d01d53cc597c4e158bd89c47891e594
Dallen Hatton <Dhatton2001@gmail.com>
2023-05-11 19:28:40 -0400

    mod control index

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 // must change for when the file is done, this is the basis of it at least. 
+
 const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes.js');
-// const dashboardRoutes = require('./dashboard-routes.js');
-// const movieSearchRoutes = require('./movie-search-routes.js');
+const userRoutes = require('./user-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+const invenSearchRoutes = require('./inven-search-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
-// router.use('/movie-search', movieSearchRoutes);
+router.use('/user', userRoutes);
+router.use('/inven-search', invenSearchRoutes);
 
 module.exports = router;
