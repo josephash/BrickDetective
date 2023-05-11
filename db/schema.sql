70bd648873664180dc48498d7bbb34a120a69c51
Sam Joseph <josephash1818@gmail.com>
2023-05-11 19:52:52 -0400

    forgot semicolons

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE inventories (
     FOREIGN KEY (set_num)
     REFERENCES sets(set_num)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE inventory_parts (
     inventory_id INT,
@@ -28,7 +28,7 @@ CREATE TABLE inventory_parts (
     FOREIGN KEY (color_id)
     REFERENCES colors(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE parts (
     part_num VARCHAR(20) NOT NULL PRIMARY KEY,
@@ -37,12 +37,12 @@ CREATE TABLE parts (
     FOREIGN KEY (part_cat_id)
     REFERENCES part_categories(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE part_categories (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(200)
-)
+);
 
 CREATE TABLE part_relationships (
     rel_type VARCHAR(1),
@@ -54,14 +54,14 @@ CREATE TABLE part_relationships (
     FOREIGN KEY (parent_part_num)
     REFERENCES parts(part_num)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE colors (
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(200),
     rgb VARCHAR(6),
     is_trans BOOLEAN DEFAULT 0
-)
+);
 
 CREATE TABLE elements (
     element_id VARCHAR(10) NOT NULL PRIMARY KEY,
@@ -73,7 +73,7 @@ CREATE TABLE elements (
     FOREIGN KEY (color_id)
     REFERENCES colors(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE inventory_minifigs (
     inventory_id INT,
@@ -85,13 +85,13 @@ CREATE TABLE inventory_minifigs (
     FOREIGN KEY (fig_num)
     REFERENCES minifigs(fig_num)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE minifigs (
     fig_num VARCHAR(20) NOT NULL PRIMARY KEY,
     name VARCHAR(256),
     num_parts INT
-)
+);
 
 CREATE TABLE inventory_sets (
     inventory_id INT,
@@ -103,7 +103,7 @@ CREATE TABLE inventory_sets (
     FOREIGN KEY (set_num)
     REFERENCES inventory_sets(set_num)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE sets (
     set_num VARCHAR(20) NOT NULL PRIMARY KEY,
@@ -114,7 +114,7 @@ CREATE TABLE sets (
     FOREIGN KEY (theme_id)
     REFERENCES themes(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE themes (
     id INT NOT NULL PRIMARY KEY,
@@ -123,7 +123,7 @@ CREATE TABLE themes (
     FOREIGN KEY (parent_id)
     REFERENCES themes(id)
     ON DELETE SET NULL
-)
+);
 
 -- User database
 CREATE TABLE users (
@@ -131,7 +131,7 @@ CREATE TABLE users (
     email VARCHAR(320) NOT NULL,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE user_inventories (
     user_id INT,
@@ -148,4 +148,4 @@ CREATE TABLE user_inventories (
     FOREIGN KEY (color_id)
     REFERENCES colors(id)
     ON DELETE SET NULL
-)
+);
