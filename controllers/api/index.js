122b1910178c4adebfa2d1e38b61be4c1489eb1e
Sam Joseph <josephash1818@gmail.com>
2023-05-21 18:43:17 -0400

    add routes (untested)

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 // Import route modules
-// const legoRoutes = require('./lego');
-// const userRoutes = require('./user');
+const legoRoutes = require('./lego');
+const userRoutes = require('./user');
 
 // Define routes
-// router.use('/user/', userRoutes);
-// router.use('/lego/', legoRoutes);
+router.use('/user', userRoutes);
+router.use('/lego', legoRoutes);
 
 module.exports = router;
--- controllers/api/user/user-routes.js
+++ controllers/api/user/user-routes.js
@@ -1,21 +1,21 @@
 const router = require("express").Router();
-const { Users, User_Inventories } = require("../../../models");
+const {
+  Themes, Sets, Inventories, Part_Categories, Parts, Colors,
+  Inventory_Parts, Part_Relationships, Elements, Minifigs,
+  Inventory_Minifigs, Inventory_Sets, Users, User_Inventories
+} = require("../../../models");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 // get all users
 router.get("/", (req, res) => {
-  User.findAll({
+  Users.findAll({
     attributes: { exclude: ["password"] },
     include: [
       {
-        model: Comment,
-        attributes: ["id", "set_id", "piece count", "user_id", "created_at"],
-        include: {
-          model: Set,
-          attributes: ["title", "theme", "piece count", ""],
-        },
-      },
+        model: Users,
+        attributes: ["id", "email", "username"]
+      }
     ],
   })
     .then((dbUserData) => res.json(dbUserData))
@@ -25,11 +25,12 @@ router.get("/", (req, res) => {
     });
 });
 
+// create user
 router.post("/", (req, res) => {
-  User.create({
-    username: req.body.username,
+  Users.create({
     email: req.body.email,
-    password: req.body.password,
+    username: req.body.username,
+    password: req.body.password
   })
     .then((dbUserData) => {
       req.session.save(() => {
@@ -37,28 +38,28 @@ router.post("/", (req, res) => {
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
 
-        let transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASS,
-          },
-        });
+        // let transporter = nodemailer.createTransport({
+        //   service: "gmail",
+        //   auth: {
+        //     user: process.env.EMAIL,
+        //     pass: process.env.PASS,
+        //   },
+        // });
 
-        let mailOptions = {
-          from: process.env.EMAIL,
-          to: dbUserData.email,
-          subject: "Hello, Friend!",
-          text: "Welcome to Brick Detective! Your new favorite site for catologing lego pieces and sets.",
-        };
+        // let mailOptions = {
+        //   from: process.env.EMAIL,
+        //   to: dbUserData.email,
+        //   subject: "Hello, Friend!",
+        //   text: "Welcome to Brick Detective! Your new favorite site for catologing lego pieces and sets.",
+        // };
 
-        transporter.sendMail(mailOptions, function (err, data) {
-          if (err) {
-            console.log("Error occured", err);
-          } else {
-            console.log("Email Sent");
-          }
-        });
+        // transporter.sendMail(mailOptions, function (err, data) {
+        //   if (err) {
+        //     console.log("Error occured", err);
+        //   } else {
+        //     console.log("Email Sent");
+        //   }
+        // });
 
         res.json(dbUserData);
       });
@@ -69,8 +70,9 @@ router.post("/", (req, res) => {
     });
 });
 
+// login
 router.post("/login", (req, res) => {
-  User.findOne({
+  Users.findOne({
     where: {
       username: req.body.username,
     },
@@ -97,6 +99,7 @@ router.post("/login", (req, res) => {
   });
 });
 
+// logout
 router.post("/logout", (req, res) => {
   if (req.session.loggedIn) {
     req.session.destroy(() => {
@@ -107,8 +110,9 @@ router.post("/logout", (req, res) => {
   }
 });
 
+// update user
 router.put("/:id", (req, res) => {
-  User.update(req.body, {
+  Users.update(req.body, {
     individualHooks: true,
     where: {
       id: req.params.id,
@@ -116,7 +120,7 @@ router.put("/:id", (req, res) => {
   })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({ message: "No user with that user ID!" });
         return;
       }
       res.json(dbUserData);
@@ -127,15 +131,16 @@ router.put("/:id", (req, res) => {
     });
 });
 
+// delete user
 router.delete("/:id", (req, res) => {
-  User.destroy({
+  Users.destroy({
     where: {
       id: req.params.id,
     },
   })
     .then((dbUserData) => {
       if (!dbUserData) {
-        res.status(404).json({ message: "No user found with this id" });
+        res.status(404).json({ message: "No user with that user ID!" });
         return;
       }
       res.json(dbUserData);
@@ -146,6 +151,123 @@ router.delete("/:id", (req, res) => {
     });
 });
 
-router.get("/id");
+// get user info
+router.get("/id", (req, res) => {
+  Users.findOne({
+    where: {
+      id: req.body.user_id,
+    },
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: Users,
+        attributes: ["id", "email", "username"]
+      }
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that user ID!" });
+        return;
+      }
+
+      res.json(dbUserData);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get all user inventories
+router.get("/inventory", (req, res) => {
+  User_Inventories.findAll({
+    include: [
+      {
+        model: User_Inventories,
+        attributes: ["user_id", "part_num", "color_id", "quantity"]
+      }
+    ],
+  })
+    .then((dbUserData) => {
+      res.json(dbUserData);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get user inventory
+router.get("/inventory/:id", (req, res) => {
+  User_Inventories.findAll({
+    where: {
+      user_id: req.params.id,
+    },
+    include: [
+      {
+        model: User_Inventories,
+        attributes: ["user_id", "part_num", "color_id", "quantity"]
+      }
+    ],
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that user ID!" });
+        return;
+      }
+
+      res.json(dbUserData);
+      return;
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/inventory/:id", (req, res) => {
+  User_Inventories.findAll({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that user ID!" });
+        return;
+      }
+      User_Inventories.findAll({
+        where: {
+          id: req.params.id,
+        },
+      })
+    });
+});
+
+router.post("/inventory/:id", (req, res) => {
+  Users.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that user ID!" });
+        return;
+      }
+      let objlist = [];
+      for (const obj of req.body.parts_list) {
+        objlist.push({
+          user_id: req.params.id,
+          ...obj
+        });
+      }
+      User_Inventories.bulkCreate(objlist);
+      return;
+    });
+});
 
 module.exports = router;
