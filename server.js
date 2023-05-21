a0e07c3bc049835a7201649af2cb98d1832cfc16
Sam Joseph <josephash1818@gmail.com>
2023-05-21 13:52:23 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-//Used to check model building:
-const User = require('./models/User');
-const Post = require('./models/Post');
-const Comment = require('./models/Comment');
+// Used to check model building:
+const Colors = require('./models/Colors');
+const Elements = require('./models/Elements');
+const Inventories = require('./models/Inventories');
+const Inventory_Minifigs = require('./models/Inventory_Minifigs');
+const Inventory_Parts = require('./models/Inventory_Parts');
+const Inventory_Sets = require('./models/Inventory_Sets');
+const Minifigs = require('./models/Minifigs');
+const Part_Categories = require('./models/Part_Categories');
+const Part_Relationships = require('./models/Part_Relationships');
+const Parts = require('./models/Parts');
+const Sets = require('./models/Sets');
+const Themes = require('./models/Themes');
+const User_Inventories = require('./models/User_Inventories');
+const Users = require('./models/Users');
 
 const sequelize = require('./config/connection'); 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
