d674ff6f6d3f6f241d74efaa4c63d2e1b35d550b
Dallen Hatton <Dhatton2001@gmail.com>
2023-05-22 21:45:52 -0400

    partial routes push

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,21 @@
-const router = require('express').Router();
+// Import dependencies
+const express = require('express');
+const router = express.Router();
 
 // Import route modules
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
-// Define routes
-router.use('/api', apiRoutes); // Handles API requests under the '/api' path
+// Set up routes
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
+// Fallback route handler
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+// Export the router
 module.exports = router;
