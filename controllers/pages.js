6b5e19246a915fe868ac58cb594b33545b2589aa
Sam Joseph <josephash1818@gmail.com>
2023-05-22 20:40:35 -0400

    homepage test

---
+++ controllers/pages.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const {
+	Themes, Sets, Inventories, Part_Categories, Parts, Colors,
+	Inventory_Parts, Part_Relationships, Elements, Minifigs,
+	Inventory_Minifigs, Inventory_Sets, Users, User_Inventories
+} = require("../../models");
+require("dotenv").config();
+
+router.get("/", (req, res) => {
+	res.render('dashboard');
+});
+
+module.exports = router;
