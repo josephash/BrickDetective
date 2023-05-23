9723511ce465d69399f03c99e48db1b08be1e890
Sam Joseph <josephash1818@gmail.com>
2023-05-22 21:27:12 -0400

    fix pages mistake

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const pages = require('./pages');
 
 // Import route modules
 const apiRoutes = require('./api');
 
 // Define routes
 router.use('/api', apiRoutes); // Handles API requests under the '/api' path
+router.use('/', pages);
 
 module.exports = router;
