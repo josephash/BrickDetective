81a7c50182b27c186b17916d63d4c277fbb836ec
Sam Joseph <josephash1818@gmail.com>
2023-05-24 21:53:48 -0400

    connect to jawsdb

--- controllers/index.js
+++ controllers/index.js
@@ -4,13 +4,14 @@ const router = express.Router();
 
 // Import route modules
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
+// const homeRoutes = require('./home-routes.js');
+// const dashboardRoutes = require('./dashboard-routes.js');
+const pagesRoutes = require('./pages.js');
 
 // Set up routes
 router.use('/api', apiRoutes);
-router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
+// router.use('/', homeRoutes);
+router.use('/dashboard', pagesRoutes);
 
 // Fallback route handler
 router.use((req, res) => {
