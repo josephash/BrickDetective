524a878ef883d9b3485dafe69a57d33b2eff6ffb
Sam Joseph <josephash1818@gmail.com>
2023-05-27 20:54:40 -0400

    fix

--- controllers/pages.js
+++ controllers/pages.js
@@ -3,7 +3,7 @@ const {
 	Themes, Sets, Inventories, Part_Categories, Parts, Colors,
 	Inventory_Parts, Part_Relationships, Elements, Minifigs,
 	Inventory_Minifigs, Inventory_Sets, Users, User_Inventories
-} = require("../../models");
+} = require("../models");
 require("dotenv").config();
 
 router.get("/", async (req, res) => {
